bd94b6c61a71ba948236c3057f54c0e6cc3122cf
ommanirai <ommanirai27@gmail.com>
2024-12-02 01:01:31 +0545

    node

---
+++ js/test.js
@@ -0,0 +1,18 @@
+console.log("hi from test file")
+
+
+// node js
+// js runtime environmnet
+/*
+command:
+1. check node version/existence
+=> node -v
+=> node --version
+
+2. run js file
+=> node filename
+
+
+
+
+*/
