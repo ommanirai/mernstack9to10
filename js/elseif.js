ea8531016ba35b6b59fdd416a637c04b3bdc85f9
ommanirai <ommanirai27@gmail.com>
2024-12-04 23:46:23 +0545

    statement

---
+++ js/elseif.js
@@ -0,0 +1,14 @@
+var n = -2342343;
+if (n % 2 === 0) {
+    console.log(n + " is even number")
+}
+else {
+    console.log(n + ' is odd number')
+}
+
+if (n > 0) {
+    console.log(n + " is positive number")
+}
+else {
+    console.log(n + " is negative number")
+}
