d6e71116f147ec97713a0fa5e04e70f6ef4837f9
ommanirai <ommanirai27@gmail.com>
2024-12-06 00:33:57 +0545

    switch

---
+++ js/switch.js
@@ -0,0 +1,78 @@
+/*
+switch
+syntax:
+switch(condition){
+    case condition1:
+        body of case condition1
+        break;
+    case condition2:
+        body of case condition2
+        break;
+    case condition3:
+        body of case condition3
+        break;
+    case condition4:
+        body of case condition4
+        break;
+    default:
+        body of default
+        break;
+}
+*/
+
+// var day = "sunday";
+// switch (day) {
+//     case "sunday":
+//         console.log("Sunday")
+//         break;
+//     case "monday":
+//         console.log("monday")
+//         break;
+//     case "tuesday":
+//         console.log("tuesday")
+//         break;
+//     case "wednesday":
+//         console.log("wednesday")
+//         break;
+//     case "thursday":
+//         console.log("thursday")
+//         break;
+//     case "friday":
+//         console.log("friday")
+//         break;
+//     case "saturday":
+//         console.log("saturday")
+//         break;
+//     default:
+//         console.log("invalid input")
+
+// }
+// prompt
+// dom
+
+var day = 10;
+switch (day) {
+    case 1:
+        console.log("Sunday")
+        break;
+    case 2:
+        console.log("monday")
+        break;
+    case 3:
+        console.log("tuesday")
+        break;
+    case 4:
+        console.log("wednesday")
+        break;
+    case 5:
+        console.log("thursday")
+        break;
+    case 6:
+        console.log("friday")
+        break;
+    case 7:
+        console.log("saturday")
+        break;
+    default:
+        console.log("invalid input")
+}
