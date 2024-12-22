0abae1577734a94f91676507c753225d45cf0fb8
ommanirai <ommanirai27@gmail.com>
2024-12-22 09:51:11 +0545

    method

---
+++ js/method.js
@@ -0,0 +1,51 @@
+/*
+method
+=> inbuilt js function
+1. setInterval()
+2. setTimeout()
+3. Number()
+4. prompt()
+5. typeof()
+
+*/
+// string
+var text = "Wel come"
+var laptop = 'acer, dell, hp, samsung'
+var n = 100
+// property
+// console.log(text.length)
+// console.log(text.toUpperCase())
+// console.log(text.toLowerCase())
+// console.log(laptop.split(","))
+// // [acer, dell,hp, samsung]
+// console.log(typeof (n))
+// // console.log(String(n))
+// console.log(typeof (String(n)))
+
+
+// number
+var a = "23";
+var num = 100.24234;
+// console.log(typeof(a))
+// console.log(typeof(Number(a)))
+// console.log(num.toFixed(0))
+
+// boolean
+// undefined
+// null
+
+// object
+var details = {
+    name: "acer",
+    ram: '4GB',
+    generation: "i7",
+}
+// for in
+for (var i in details) {
+    // console.log(i)
+    // console.log(details)
+    // console.log(details[i])
+    // details['name']
+    // details['ram']
+    // details['generation']
+}
